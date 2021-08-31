9f542b788571595a8a2509fed97c6bada6ec7a48
Lam <nl2791999@gmail.com>
2021-08-30 20:49:03 -0500

    First commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 const mongoose = require('mongoose')
 var session = require('express-session')
 var FileStore = require('session-file-store')(session)
+var passport = require('passport')
+var authenticate = reuiqre('./authenticate')
+
 
 const Dishes = require('./models/dishes')
 
@@ -45,24 +48,21 @@ app.use(session({
     store: new FileStore()
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
     console.log(req.session);
 
-    if (!req.session.user) {
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
         err.status = 403;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
+        next()
     }
 }
 
