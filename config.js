90a65517afccfb18230e3fd108557c05f68b59f1
Lam <nl2791999@gmail.com>
2021-09-08 15:55:54 -0500

    fix key

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1704926936373503',
+        clientSecret: 'e5fd9559095cb3a4bd95da8bcb1be9a1'
+
+    }
 }
